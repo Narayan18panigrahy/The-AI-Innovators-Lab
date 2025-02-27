c7e4b6348c6efa660454c80f17688388755abec5
Rohit Ranjan <29282712+Itzsrohit@users.noreply.github.com>
2025-02-27 21:24:00 +0530

    feat(NerAnalysisSection): Add SimpleTable component import

--- NextGen Dataops/frontend/src/sections/NerAnalysisSection.js
+++ NextGen Dataops/frontend/src/sections/NerAnalysisSection.js
@@ -167,26 +167,3 @@ function NerAnalysisSection({ profileReport, nerReport, setNerReport, setLoading
                     {isLoading ? <><Spinner size="sm" /> Analyzing...</> : 'Run NER Analysis'}
                 </Button>
             </Form>
-
-            {/* Display NER Results */}
-            {nerReport && typeof nerReport === 'object' && Object.keys(nerReport).length > 0 && (
-                <div className="mt-2">
-                    <h5 className="mb-3">Analysis Results:</h5>
-                    {/* Use Accordion to make results per column collapsible */}
-                    <Accordion>
-                        {Object.entries(nerReport).map(([colName, colData], index) => (
-                             <Accordion.Item eventKey={String(index)} key={colName}>
-                                 <Accordion.Header>Column: '{colName}'</Accordion.Header>
-                                 <Accordion.Body>
-                                     {renderColumnNerResults(colName, colData)}
-                                 </Accordion.Body>
-                             </Accordion.Item>
-                        ))}
-                    </Accordion>
-                </div>
-            )}
-        </div>
-    );
-}
-
-export default NerAnalysisSection;
